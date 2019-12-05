93db8872d3a5501ab235858c80ed6b8a759dff09
BrunoFgR <brunofds281@gmail.com>
2019-12-05 12:49:44 -0300

    Finalizando tela registration/Dashboard

--- src/pages/registration/Dashboard/index.js
+++ src/pages/registration/Dashboard/index.js
@@ -1,20 +1,78 @@
 import React from 'react';
-import { MdAdd } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { MdAdd, MdCheckCircle } from 'react-icons/md';
+import history from '~/services/history';
 
-import { Container, Info, Controller } from './styles';
+import { Container, Info, Table } from './styles';
 
 export default function Dashboard() {
   return (
     <Container>
       <Info>
-        <h1>Gerenciando alunos</h1>
-        <Controller>
-          <button type="button">
-            <MdAdd color="#fff" size={20} />
-            CADASTRAR
-          </button>
-        </Controller>
+        <h1>Gerenciando matrículas</h1>
+
+        <button
+          type="button"
+          onClick={() => history.push('/registrations/new')}
+        >
+          <MdAdd color="#fff" size={20} />
+          CADASTRAR
+        </button>
       </Info>
+      <Table>
+        <table>
+          <thead>
+            <tr>
+              <th>NOME</th>
+              <th>PLANO</th>
+              <th>INICIO</th>
+              <th>TÉRMINO</th>
+              <th>ATIVA</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Bruno Figueiredo</td>
+              <td>Start</td>
+              <td>05 de Dezembro de 2019</td>
+              <td>05 de Janeiro de 2020</td>
+              <td>
+                <MdCheckCircle size={20} color="#42CB59" />
+              </td>
+              <td>
+                <Link to="/registrations/edit/1">editar</Link>
+                <button type="button">remover</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Bruno Figueiredo</td>
+              <td>Start</td>
+              <td>05 de Dezembro de 2019</td>
+              <td>05 de Janeiro de 2020</td>
+              <td>
+                <MdCheckCircle size={20} color="#42CB59" />
+              </td>
+              <td>
+                <Link to="/registrations/edit/1">editar</Link>
+                <button type="button">remover</button>
+              </td>
+            </tr>
+            <tr>
+              <td>Bruno Figueiredo</td>
+              <td>Start</td>
+              <td>05 de Dezembro de 2019</td>
+              <td>05 de Janeiro de 2020</td>
+              <td>
+                <MdCheckCircle size={20} color="#42CB59" />
+              </td>
+              <td>
+                <Link to="/registrations/edit/1">editar</Link>
+                <button type="button">remover</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </Table>
     </Container>
   );
 }
--- src/pages/registration/Dashboard/styles.js
+++ src/pages/registration/Dashboard/styles.js
@@ -1,7 +1,101 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
-export const Info = styled.div``;
+export const Info = styled.div`
+  width: 90%;
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-export const Controller = styled.div``;
+  h1 {
+    font-size: 2.6rem;
+    color: #444;
+  }
+
+  button {
+    background: #ee4d64;
+    color: #fff;
+    padding: 10px 15px;
+    border-radius: 4px;
+    border: 0;
+    font-weight: bold;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 5px;
+    }
+  }
+`;
+
+export const Table = styled.div`
+  width: 90%;
+
+  table {
+    background: #fff;
+    width: 100%;
+    margin-top: 20px;
+    border-radius: 4px;
+    padding: 30px;
+
+    th {
+      text-align: center;
+      font-size: 1.6rem;
+      color: #444;
+      padding-bottom: 20px;
+
+      &:first-child {
+        text-align: start;
+      }
+    }
+
+    tbody {
+      tr:first-child {
+        td {
+          padding-top: 0;
+        }
+      }
+
+      tr:last-child {
+        td {
+          border: 0;
+          padding-bottom: 0;
+        }
+      }
+
+      td {
+        font-size: 1.6rem;
+        text-align: center;
+        color: #666;
+        padding: 15px 0;
+        border-bottom: 1px solid #eee;
+
+        &:last-child {
+          text-align: end;
+        }
+
+        &:first-child {
+          text-align: start;
+        }
+
+        button {
+          background: #fff;
+          border: 0;
+          color: #de3b3b;
+          margin-left: 20px;
+        }
+
+        a {
+          color: #4d85ee;
+        }
+      }
+    }
+  }
+`;
